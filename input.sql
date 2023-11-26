d6b3cdbc102dc42bfe773f126bde2985095a2ac7
GOHU <dev.gohu@gmail.com>
2023-11-27 07:34:48 +0900

    input.sql update

--- input.sql
+++ input.sql
@@ -1,1 +1,4 @@
-INPUT
+INSERT INTO `고객`(`기업명`, `사업자등록번호`, `전화번호`, `email`, `주소`, `FAX`) 
+VALUES ("고기먹고싶다", "123-01-15379", "0215881588", "gogi@chicken.com", "서울 송파구 거마로 10, 203호", "FAX=279-167-1673@chicken.com")
+
+INSERT INTO 
